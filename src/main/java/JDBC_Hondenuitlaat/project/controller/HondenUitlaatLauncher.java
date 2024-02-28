668a6dcef985398bdec3c5c5bfcab1981edbc6dd
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-02-28 16:41:40 +0100

    create startproject

--- src/main/java/JDBC_Hondenuitlaat/project/controller/HondenUitlaatLauncher.java
+++ src/main/java/JDBC_Hondenuitlaat/project/controller/HondenUitlaatLauncher.java
@@ -11,39 +11,10 @@ import java.util.ArrayList;
 public class HondenUitlaatLauncher {
 
     public static void main(String[] args) {
-        DBAccess dBaccess =  new DBAccess("UitlaatService", "root", "45HQDAZpUuyqrL");
-        CustomerDAO customerDAO = new CustomerDAO(dBaccess);
-        DogDAO dogDAO = new DogDAO(dBaccess);
-
+        DBAccess dBaccess = new DBAccess("UitlaatService", "userUitlaatService", "pwUitlaatService");
         dBaccess.openConnection();
 
-        // Klant testen
-        System.out.println("Testen van klantDAO methodes: ");
-        customerDAO.storeCustomer(new Customer(6, "O.B.", null, "Bommel", "098712345"));
-        Customer customer = customerDAO.getCustomerById(2);
-        System.out.println("Klant ophalen: " + customer);
-        System.out.println();
-        System.out.println("Alle klanten ophalen: ");
-        ArrayList<Customer> customerList = customerDAO.getAllCustomers();
-        for (Customer customer1 : customerList) {
-            System.out.println(customer1);
-        }
-        // Hond testen
-        System.out.println();
-        System.out.println("Testen van hondDAO methods: ");
-        Dog dog = dogDAO.getDogById("3");
-        System.out.println("Hond ophalen: " + dog);
-        ArrayList<Dog> dogList = dogDAO.getDogsByCustomer(customer);
-        System.out.println();
-        System.out.println("Hond van klant ophalen");
-        for (Dog dog1 : dogList) {
-            System.out.println(dog1);
-        }
-        System.out.println();
-        System.out.println("Alle honden ophalen: ");
-        dogList = dogDAO.getAllDogs();
-        for (Dog dog1 : dogList) {
-            System.out.println(dog1);
-        }
+
+        dBaccess.closeConnection();
     }
 }
--- src/main/java/JDBC_Hondenuitlaat/project/database/CustomerDAO.java
+++ src/main/java/JDBC_Hondenuitlaat/project/database/CustomerDAO.java
@@ -16,63 +16,13 @@ public class CustomerDAO {
     }
 
     public void storeCustomer(Customer mpCustomer) {
-        String sql = "INSERT INTO Klant VALUES (?,?,?,?,?) ;";
-        try {
-            PreparedStatement preparedStatement = dBaccess.getConnection().prepareStatement(sql);
-            preparedStatement.setInt(1, mpCustomer.getCustomerNumber());
-            preparedStatement.setString(2, mpCustomer.getInitials());
-            preparedStatement.setString(3, mpCustomer.getPrefix());
-            preparedStatement.setString(4, mpCustomer.getSurname());
-            preparedStatement.setString(5, mpCustomer.getPhoneNumber());
-            preparedStatement.executeUpdate();
-        } catch (SQLException sqlFout) {
-            System.out.println("SQL fout " + sqlFout.getMessage());
-        }
     }
 
     public Customer getCustomerById(int customerNumber) {
-        String sql = "SELECT * FROM Klant WHERE klantnr = ?";
-        Customer result = null;
-        try {
-            PreparedStatement preparedStatement = dBaccess.getConnection().prepareStatement(sql);
-            preparedStatement.setInt(1, customerNumber);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                String initials = resultSet.getString("voorletters");
-                String prefix = resultSet.getString("voorvoegsels");
-                String surname = resultSet.getString("achternaam");
-                String phoneNumber = resultSet.getString("telefoon");
-                result = new Customer(customerNumber, initials, prefix, surname,  phoneNumber);
-            } else {
-                System.out.println("Klant met dit klantnummer bestaat niet");
-            }
-        }
-        catch (SQLException sqlFout){
-            System.out.println("SQL fout " + sqlFout.getMessage());
-        }
-        return result;
+        return new Customer(0,"","","","");
     }
 
     public ArrayList<Customer> getAllCustomers() {
-        ArrayList<Customer> resultList = new ArrayList<>();
-        String sql = "SELECT * FROM Klant";
-        Customer customer;
-        try {
-            PreparedStatement preparedStatement = dBaccess.getConnection().prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                int klantnummer = resultSet.getInt("klantnr");
-                String initials = resultSet.getString("voorletters");
-                String prefix = resultSet.getString("voorvoegsels");
-                String surname = resultSet.getString("achternaam");
-                String phoneNumber = resultSet.getString("telefoon");
-                customer = new Customer(klantnummer, initials, prefix, surname,  phoneNumber);
-                resultList.add(customer);
-            }
-        }
-        catch (SQLException sqlFout){
-            System.out.println("SQL fout " + sqlFout.getMessage());
-        }
-        return resultList;
+        return new ArrayList<>();
     }
 }
